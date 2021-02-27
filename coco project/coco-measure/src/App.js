788cf7716f58c550f13306361884740b44a81dfb
manodya99 <manodya.2018629@iit.ac.lk>
2021-02-27 09:07:16 +0530

    second commit

---
+++ coco project/coco-measure/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="App">
+
+      <nav>
+      <ul>
+            <li>
+                <a href="Home.html">[][][]</a>               
+                <ul>
+                    <li><a href=""> Help ?</a></li>
+                    <li><a href=""> FAQ</a></li>
+                    <li><a href=""> Gazette</a></li>
+                    <li><a href=""> About Us</a></li>
+
+                </ul>
+            </li> 
+        </ul>
+
+      </nav>
+      
+      <div className="content">
+        
+        <h1>Coco Measure</h1>
+
+        <button className="btn">Upload Image</button>
+        <button className="btn">Capture Photo</button>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
