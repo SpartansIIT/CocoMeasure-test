7f3f1fc170a7f8734177b8c804b77f0c17b93d98
manodya99 <61262245+manodya99@users.noreply.github.com>
2021-03-08 19:21:00 +0530

    Add files via upload

---
+++ coco project/coco-measure/src/Gazette.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+
+function Gazette(){
+    return(
+        <div className="content">
+
+          
+        <h1 className ="h1">Coco </h1>
+
+        <button className="btn">Upload</button>
+        <button className="btn">Capture</button>
+
+
+      </div>
+  );
+}
+
+export default Gazette;
